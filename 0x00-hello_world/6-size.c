0f67fef8e6beaa955a9cc67528e81db4437b0b79
Nandwa <nandwajapheth7@gmail.com>
2023-10-06 06:33:13 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("size of a char: %1d byte(s)\n", sizeof(char));
-printf("size of an int: %1d byte(s)\n", sizeof(int));
-printf("size of a long int: %1d byte(s)\n", sizeof(long int));
-printf("size of a long long  int: %1d byte(s)\n", sizeof(long long int));
-printf("size of a float: %1d byte(s)\n", sizeof(float));
+printf("size of a char: %i byte(s)\n", sizeof(char));
+printf("size of an int: %i byte(s)\n", sizeof(int));
+printf("size of a long int: %i byte(s)\n", sizeof(long int));
+printf("size of a long long  int: %i byte(s)\n", sizeof(long long int));
+printf("size of a float: %i byte(s)\n", sizeof(float));
 return (0);
 }
