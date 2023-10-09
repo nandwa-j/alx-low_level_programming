1a98322d91f446feeb4c27175ce7faf8a6f7ff93
Nandwa <nandwajapheth7@gmail.com>
2023-10-09 07:53:50 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,4 +6,25 @@
  */
 int main(void)
 {
-int num;
+int x;
+int y;
+
+for (x = 0; x < 99; x++)
+{
+for (y = (x + 1); y < 100; y++)
+{
+putchar('0' + (x / 10));
+putchar('0' + (x % 10));
+putchar(' ');
+putchar('0' + (y / 10));
+putchar('0' + (y % 10));
+if (!(x == 98 && y == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
