3d986fafeba958956ccc4901b4b1f6069f0f33a2
Nandwa <nandwajapheth7@gmail.com>
2023-10-11 11:49:33 -0700

    4

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_alphabet - Print the alphabet in lowercase
  *
- * Return: Always 0 (success)
  */
 void print_alphabet(void)
 {
 char c; 
+c = 'a';
 
 for (c = 97; c <= 122; c++)
 {
-putchar(c);
+_putchar(c);
 }
-putchar('\n');
+_putchar('\n');
 }
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,24 @@
 #include "main.h"
-?
+#include <stdio.h>
 /**
- * print_last_digit - Prints the last digit of a number
- * @j - int type number
- * Returns: The value of the last digit
+ * _abs - Computes the absolute value of an integer
+ * @i: int type number
+ * 
+ * Returns: The absolute value of @i
+ * main - Entry
  */
-int print_last_digit(int j)
+int _abs(int i)
 {
+int i;
+if (i < 0)
+{
+return (i * -1);
+}
+else
+{
+return (i);
+}
 
+int main(void)
+{
+}
