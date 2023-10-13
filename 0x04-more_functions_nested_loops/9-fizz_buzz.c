7e889a73f92b51ee7330a05f8f27030bc07e59ed
Nandwa <nandwajapheth7@gmail.com>
2023-10-13 14:12:34 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,19 @@ for (a = 1; a <= 100; a++)
 {
 if (a % 3 == 0 && a % 5 == 0)
 {
-printf(" FizzBuzz");
+printf("FizzBuzz ");
 }
 else if (a % 3 == 0)
 {
-printf(" Fizz");
+printf("Fizz ");
 }
 else if (a % 5 == 0)
 {
-printf(" Buzz");
+printf("Buzz ");
 }
 else
 {
-printf(" %d", a);
+printf("%d ", a);
 }
 }
 printf("\n");
