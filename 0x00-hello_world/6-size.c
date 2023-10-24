62733e1315febe75626249eeafa100a5bdb4ad98
Nandwa <nandwajapheth7@gmail.com>
2023-10-24 14:43:07 +0300

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdint.h>
 /**
  * main - Entry point
  *
@@ -16,6 +17,6 @@ printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
