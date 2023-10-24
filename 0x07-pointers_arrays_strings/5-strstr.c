5ecb5b082d1ab540a1e3cf048f9391fc4b33d0bb
Nandwa <nandwajapheth7@gmail.com>
2023-10-24 22:56:43 +0300

    changes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,22 +8,23 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int a, b, c;
-a = 0;
-c = 0;
-while (haystack[a] != '\0')
-b = 0;
-while (needle[b + c] != '\0' && haystack[a + c] !='\0' && needle[b + c] == haystack[a + c])
+int a = 0, b;
+if (needle[0] == '\0')
 {
-if (haystack[a + c] != needle[b + c])
-break;
-c++;
+return (haystack);
 }
-if (needle[b + c] == '\0')
+while (haystack[a] != '\0')
 {
-return (&haystack[a]);
-a++;
+b = 0;
+while (needle[a + b] != needle[b])
+{
+if (haystack[b + 1] == '\0')
+{
+return (haystack + a);
+}
 b++;
+}
+a++;
 }
 return (NULL);
 }
