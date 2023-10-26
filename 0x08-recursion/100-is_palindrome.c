aefb70a094648c3816485c7c32e87535b52758f4
Nandwa <nandwajapheth7@gmail.com>
2023-10-27 00:07:13 +0300

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,34 +1,44 @@
 #include "main.h"
 /**
- * check_pali - checks if a string is a palindrome
- * @head: pointer to the start of the string
- * @tail: pointer to the end of the string
- * Return: 0 if it's not a palindrome, 1 if it is
+ * get_length - gets length of string
+ * @str: pointer to a string
+ * @len: length of a string
+ * Return: return length of string
  */
-int check_pali(char *head, char *tail)
+int get_length(char *str, int len)
 {
-if (head >= tail)
+if (*(str + len) != '\0')
 {
-return (1);
+return (get_length(str, ++len));
 }
-if (*head == *tail)
-{
-return (check_pali(head + 1, tail - 1));
+return (--len);
 }
+/**
+ * check_pali - gets length of string
+ * @a: pointer to a string
+ * @b: length of a string
+ * @len: length of a string
+ * Return: return 0 if its not palindrome return 1 if it is
+ */
+int check_pali(char *a, char *b, int len)
+{
+if (*a == *(b + len) && len > 1)
+{
+a++;
+return (check_pali(a, b, --len));
+}
+if (len == 1)
+{
 return (0);
+}
+return (1);
 }
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: pointer to a string
- * Return: 0 if it's not a palindrome, 1 if it is
+ * Return: return 0 if its not palindrome return 1 if it is
  */
 int is_palindrome(char *s)
 {
-char *tail = s;
-while (*tail)
-{
-tail++;
-tail--;
-}
-return (check_pali(s, tail));
+return (check_pali(s, s, get_length(s, 0)));
 }
