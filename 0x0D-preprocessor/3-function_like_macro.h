cfdaa1bd0c8d65c8d9f97095b8e9be3b93d6184c
Nandwa <nandwajapheth7@gmail.com>
2023-11-06 22:22:14 +0300

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,5 @@
-#ifndef 3_FUNCTION_LIKE_MACRO_H
-#define 3_FUNCTION_LIKE_MACRO_H
+#ifndef _3_FUNCTION_LIKE_MACRO_H
+#define _3_FUNCTION_LIKE_MACRO_H
 #define ABS(x) (x > 0 ? (x) : (x) * -1)
+typedef int make_iso_compilers_happy;
 #endif /*3_FUNCTION_LIKE_MACRO_H*/
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,4 +1,5 @@
-#ifndef 4_SUM_H
-#define 4_SUM_H
+#ifndef _4_SUM_H
+#define _4_SUM_H
 #define SUM(x, y) ((x) + (y))
+typedef int make_iso_compilers_happy;
 #endif /*4_SUM_H*/
