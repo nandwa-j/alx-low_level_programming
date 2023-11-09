b107902af58663b1e19e64a0a145e2327fabb187
Nandwa <nandwajapheth7@gmail.com>
2023-11-09 23:35:15 +0300

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,19 +16,27 @@ while (format && format[i])
 {
 if (c && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
 printf(", ");
+
 switch (format[i])
 {
 case 'c':
 printf("%c", va_arg(valist, int));
+c = 1;
 break;
+
 case 'i':
 printf("%d", va_arg(valist, int));
+c = 1;
 break;
+
 case 'f':
 printf("%f", va_arg(valist, double));
+c = 1;
 break;
+
 case 's':
 str = va_arg(valist, char *);
+c = 1;
 if (str == NULL)
 str = "(nil)";
 printf("%s", str);
