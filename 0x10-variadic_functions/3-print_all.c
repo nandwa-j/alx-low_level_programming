60779b208df4e64a041ab795f4c71a4d0730f831
Nandwa <nandwajapheth7@gmail.com>
2023-11-09 22:48:07 +0300

    variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,32 +8,28 @@
 void print_all(const char * const format, ...)
 {
 va_list valist;
-unsigned int i = 0;
+unsigned int i = 0, middle = 0;
 char *str;
 int c = 0;
 va_start(valist, format);
 while (format && format[i])
 {
-if (c && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+if (middle)
 printf(", ");
-
 switch (format[i])
 {
 case 'c':
 printf("%c", va_arg(valist, int));
 c = 1;
 break;
-
 case 'i':
 printf("%d", va_arg(valist, int));
 c = 1;
 break;
-
 case 'f':
 printf("%f", va_arg(valist, double));
 c = 1;
 break;
-
 case 's':
 str = va_arg(valist, char *);
 c = 1;
@@ -41,7 +37,12 @@ if (str == NULL)
 str = "(nil)";
 printf("%s", str);
 break;
+default:
+middle = 0;
+i++;
+continue;
 }
+middle = 1;
 i++;
 }
 va_end(valist);
