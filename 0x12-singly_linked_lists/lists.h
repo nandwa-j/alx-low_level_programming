37d1d94b4689ebe9ab78aa5a589aef0ea9f8dd44
Nandwa <nandwajapheth7@gmail.com>
2023-11-17 08:48:33 +0300

    singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,18 +3,17 @@
 
 #include <stddef.h>
 /**
- * struct list_s - sinly linked list
+ * struct list_s - singly linked list
  * @str: string
- * @len length of the string
+ * @len: length of the string
  * @next: points to the next node
- * @description: node structure of sinlgy linked lists
  */
 typedef struct list_s
 {
 char *str;
 unsigned int len;
 struct list_s *next;
-}list_t;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
