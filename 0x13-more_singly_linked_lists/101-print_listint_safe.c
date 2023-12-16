c03562fee3fce0ef5ee64ed0e4be22f63214df65
Nandwa Japheth <japhethnandwa7@gmail.com>
2023-12-16 03:42:27 +0300

    additions

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,52 @@
 #include "lists.h"
+
 /**
  * print_listint_safe - function that prints a listint_t linked list
- * @head: begining of the list
+ * @head: beginning of the list
  * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *slow, *fast;
-int flag = 0, i = 0;
-slow = head;
-fast = head;
-if (!head)
-{
-exit(98);
+	const listint_t *slow, *fast;
+	int flag = 0, i = 0;
+
+	slow = head;
+	fast = head;
+	if (!head)
+	{
+		exit(98);
+	}
+	while (fast && fast->next && !flag)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			flag = 1;
+		}
+	}
+	if (flag)
+	{
+		slow = head;
+		while (slow != fast)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			i++;
+			slow = slow->next;
+			fast = fast->next;
+		}
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("-> [%p] %d\n", (void *)fast, fast->n);
+	}
+	else
+	{
+		while (head)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			i++;
+			head = head->next;
+		}
+	}
+	return (i);
 }
 
-while (fast && fast->next && !flag)
-{
-slow = slow->next;
-fast = fast->next->next;
-if (slow == fast)
-flag = 1;
-}
-for (; flag && head != fast; i++, head = head->next)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-}
-printf("[%p] %d\n", (void *)head, head->n);
-if (flag)
-printf("-> [%p] %d\n", (void *)fast, fast->n);
-return (i + 1);
-}
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,45 +2,56 @@
 #include <stdlib.h>
 
 /**
- * free_listint_safe - function that free a listint_t linked list
+ * free_listint_safe - function that frees a listint_t linked list
  * @h: pointer to the beginning of linked list
  * Return: the number of nodes in the list
  */
 
 size_t free_listint_safe(listint_t **h)
 {
-int a, flag = 0;
-listint_t *slow, *fast, *delete;
-if (!h)
-return (0);
-for (a = 0; *h && !flag; a++)
-{
-slow = *h;
-fast = (**h).next;
-while (slow != fast)
-{
-if (slow)
-slow = (*slow).next;
-if (fast)
-fast = (*fast).next;
-if (fast == *h)
-flag = 1;
-if (fast)
-fast = (*fast).next;
-if (fast == *h)
-flag = 1;
+	int a;
+	listint_t *slow, *fast, *delete;
+
+	if (!h || !*h)
+		return (0);
+
+	for (a = 0; *h; a++)
+	{
+		slow = *h;
+		fast = (*h)->next;
+
+		while (slow != fast)
+		{
+			if (slow)
+				slow = slow->next;
+			if (fast)
+				fast = fast->next;
+			if (fast == *h)
+				break;
+			if (fast)
+				fast = fast->next;
+			if (fast == *h)
+				break;
+		}
+
+		if (fast == *h)
+		{
+			slow = *h;
+			fast = (*h)->next;
+			while (slow != fast)
+			{
+				delete = slow;
+				slow = slow->next;
+				free(delete);
+			}
+			free(slow);
+			break;
+		}
+		delete = *h;
+		*h = (*h)->next;
+		free(delete);
+	}
+	*h = NULL;
+	return (a);
 }
-delete = *h;
-*h = (**h).next;
-free(delete);
-}
-while (flag && *h != fast)
-{
-delete = *h;
-a++;
-*h = (**h).next;
-free(delete);
-}
-*h = NULL;
-return (a);
-}
+
