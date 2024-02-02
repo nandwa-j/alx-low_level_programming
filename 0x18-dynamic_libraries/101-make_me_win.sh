640a4f4257af56ae91b8762b228627903498d4f0
Yukihira <Nandwa Japheth>
2024-02-02 22:41:02 +0300

    solutions to hash tables tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/nandwa-j/alx-low_level_programming/master/0x18-dynamic_libraries/puts.so
-export LD_PRELOAD="$PWD/../puts.so"
+wget -P .. https://raw.githubusercontent.com/nandwa-j/alx-low_level_programming/master/0x18-dynamic_libraries/1.so
+export LD_PRELOAD="$PWD/../1.so"
